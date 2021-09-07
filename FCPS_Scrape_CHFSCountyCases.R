5a3ca70de8e4ea0c235960ec00b9631a8f9d53c7
Darren W Henderson <dwhend0@gmail.com>
2021-09-07 13:33:07 -0400

    change union to rbind - unpredictable behavior

--- FCPS_Scrape_CHFSCountyCases.R
+++ FCPS_Scrape_CHFSCountyCases.R
@@ -5,12 +5,15 @@ library(httr)
 
 casesCSV <- "KYDailyCases.csv"
 
+lastDate <- as.Date("2021-01-01","%Y-%m-%d")
+
 if(file.exists(casesCSV)){
   KYDailyCases <- read.csv(casesCSV,header=T,sep=",")
-  lastDate <- max(as.Date(KYDailyCases$"Date","%Y-%m-%d"), na.rm=F)
-} else {
-  lastDate <- as.Date("2021-01-01","%Y-%m-%d")
-}
+  if(max(as.Date(KYDailyCases$"Date","%Y-%m-%d"), na.rm=F) > lastDate)
+  {
+    lastDate <- max(as.Date(KYDailyCases$"Date","%Y-%m-%d"), na.rm=F)
+  }
+} 
 
 tryPDFYY <- seq(lastDate+1,Sys.Date(), by="days") %>% as.character("%m%d%y")
 baseURL <- "https://chfs.ky.gov/cvdaily/COVID19DailyReport"
@@ -19,32 +22,37 @@ tryPDFYYYY <- seq(lastDate+1,Sys.Date(), by="days") %>% as.character("%m%d%Y")
 tryURLYYYY <- paste(baseURL,tryPDFYYYY,".pdf",sep="")
 
 lapply(tryURLYY,function(pYY){
-  tryCatch({
-    res<-GET(pYYYY,SSL_VERIFY_PEER=F)
-    if(res$status_code==200){
-      bin<-content(RES,"raw")
-      writeBin(bin,str_replace(pYYYY,"https://chfs.ky.gov/cvdaily/","./KYDailyCovidReports/"))
+  if(!(file.exists(str_replace(pYY,"https://chfs.ky.gov/cvdaily/","./KYDailyCovidReports/")))){
+    tryCatch({
+      res<-GET(pYY,SSL_VERIFY_PEER=F)
+      if(res$status_code==200){
+        bin<-content(res,"raw")
+        writeBin(bin,str_replace(pYY,"https://chfs.ky.gov/cvdaily/","./KYDailyCovidReports/"))
+        }
       }
-    }
-    ,error = function(e) print(paste(pYY, "not available")))
+      ,error = function(e) print(paste(pYY, "not available")))
+  }
 })
 
 lapply(tryURLYYYY,function(pYYYY){
+  if(!(file.exists(str_replace(pYYYY,"https://chfs.ky.gov/cvdaily/","./KYDailyCovidReports/")))){
   tryCatch({
     res<-GET(pYYYY,SSL_VERIFY_PEER=F)
     if(res$status_code==200){
-      bin<-content(RES,"raw")
-      writeBin(bin,str_replace(pYYYY,"https://chfs.ky.gov/cvdaily/","./KYDailyCovidReports/"))
+      bin<-content(res,"raw")
+      writeBin(bin,str_replace(str_replace(pYYYY,"https://chfs.ky.gov/cvdaily/","./KYDailyCovidReports/"),"2021.pdf","21.pdf"))
       }
     }
     ,error = function(e) print(paste(pYYYY, "not available"))
     )
+  }
 })
 
 fileList <- list.files("./KYDailyCovidReports/", pattern="*.pdf", full.names=T, recursive=F)
 
 lapply(fileList,function(f) {
   fileDate <- str_replace(str_replace(f,"./KYDailyCovidReports/COVID19DailyReport",""),".pdf","") %>% as.Date("%m%d%y")
+
   if(fileDate > lastDate){
     newCaseTable <- extract_tables(f,pages=4,output="data.frame")
 
@@ -60,9 +68,9 @@ lapply(fileList,function(f) {
     c3df <- data.frame(Date=fileDate,
                        County=newCaseTable[[1]]$County.3,
                        Cases= as.numeric(newCaseTable[[1]]$Cases.3))
-    cases <- union(union(c0df,c1df),union(c2df,c3df))
+    cases <- rbind(rbind(c0df,c1df),rbind(c2df,c3df))
     
-      if(file.exists(casesCSV)){
+    if(file.exists(casesCSV)){
         write.table(cases,file="KYDailyCases.csv",sep=",",append=T,row.names=F,col.names=F)
       } else {
         write.table(cases,file="KYDailyCases.csv",sep=",",append=T,row.names=F,col.names=T)
